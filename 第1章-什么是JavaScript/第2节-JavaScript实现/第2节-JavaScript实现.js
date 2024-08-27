bba55d68827187223e2dbb06221dd32fe6a198bf
liuyy <liuyychn@outlook.com>
2024-08-27 21:50:09 +0800

    1.2 JavaScript 实现描述

---
+++ 第1章-什么是JavaScript/第2节-JavaScript实现/第2节-JavaScript实现.js
@@ -0,0 +1,12 @@
+console.log(`
+    
+    1.2 JavaScript 实现
+
+        虽然 JavaScript 和 ECMAScript 基本上是同义词，但 JavaScript 远远不限于
+    ECMA-262 所定义的那样。没错，完整的 JavaScript 实现包含以下几个部分：
+
+        · 核心（ECMASCript）
+        · 文档对象模型（DOM）
+        · 浏览器对象模型（BOM）
+
+    `);
