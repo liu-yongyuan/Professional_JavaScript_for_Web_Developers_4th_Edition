e1f2982ff89b1d8139d3ced4c8a1271e01bb1f08
liuyy <liuyychn@outlook.com>
2024-08-27 21:48:03 +0800

    1.1 简短的历史回顾

---
+++ 第1章-什么是JavaScript/第1节-简短的历史回顾/1-1-简短的历史回顾.js
@@ -0,0 +1,30 @@
+console.log(`
+    
+    1.1 简短的历史回顾
+
+        1995年 brendan eich 的工程师，为即将发布的 Netscape Navigator2 开发一个叫
+    Mocha（后来改名为 LiveScript）的脚本语言。
+
+        为了赶上发布时间，网景与 Sun 公司结尾开发联盟，共同完成了 LiveScript 的开发。
+    并把 LiveScript 改名为 JavaScript。
+
+        由于 JavaScript1.0 很成功，网景又在 Netscape Navigator 3 中发布了 1.1 版本。
+    就在 Netscape Navigator 3 发布后不久，微软发布了 IE ，其中包含自己命名为 JScript
+    的 JavaScript 实现。1996 年 8 月，微软重磅进入 Web 浏览器领域，这是网景永远的痛，
+    但它代表 JavaScript 作为一门语言向前迈进了一大步。
+
+        1997 年，JavaScript 1.1 作为提案被提交给欧洲计算机制造商协会（Ecma）。
+    第 39 技术委员会（TC39）承担了“标准化一门通用、跨平台、厂商中立的脚本语言的语法和语义”
+    的任务（TC39-ECMAScript）。TC39 委员会来自网景、Sun、微软、Borland、Nombas 和其他对
+    这门脚本语言有兴趣的工程师组成。他们花了数月时间打造出 ECMA-262，也就是ECMAScript 这个
+    新的脚本语言标准。
+
+        1998 年，国际化标准化组织（ISO）和国际电工委员会（IEC）也将ECMAScript 采纳为标准
+    （ISO/IEC-16262）。自此以后，各家浏览器均以 ECMAScript 作为自己 JavaScript 实现的依据，
+    虽然具体实现各有不同。
+
+
+    更详细的可以参考 《JavaScript 二十年》书中详细描述了 JavaScript 发展过程：
+    https://github.com/doodlewind/jshistory-cn
+
+    `)
