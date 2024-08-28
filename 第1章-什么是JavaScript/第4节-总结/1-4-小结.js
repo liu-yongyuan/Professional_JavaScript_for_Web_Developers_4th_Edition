f2d562542f5610e8910e0283f8013ae669d18e9c
liuyy <liuyychn@outlook.com>
2024-08-28 17:37:28 +0800

    第1章小结，完成第1章阅读

---
+++ 第1章-什么是JavaScript/第4节-总结/1-4-小结.js
@@ -0,0 +1,15 @@
+console.log(`
+    
+    1.4 小结
+
+    JavaScript 是一门用来与网页交互的脚本语言，包含以下三个组成部分。
+        · ECMAScript：由ECMA-262 定义并提供核心功能。
+        · 文档对象模型（DOM）：提供与网页内容交互的方法和接口。
+        · 浏览器对象模型（BOM）：提供与浏览器交互的方法和接口。
+
+    JavaScript 的这三个部分得到了五大 Web 浏览器（IE、Firefox、Chrome、Safari 和 Opera）不同程度
+    的支持。所有浏览器基本上对ES5（ECMAScrip 5）提供了完善的支持，而对 ES6（ECMAScript 6）和 ES7 的
+    支持度也在不断的提升。这些浏览器对 DOM 的支持各不相同，但对 Level3 的支持日益趋于规范。HTML5 中
+    收录的 BOM 会因为浏览器而异，不过开发者仍然可以假定存在很大一部分公共特性。
+
+    `);
