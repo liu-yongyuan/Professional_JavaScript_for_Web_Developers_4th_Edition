c2fd7b5808ea86d25dc0026455d9bde9a9903f17
liuyy <liuyychn@outlook.com>
2024-08-30 15:28:45 +0800

    关键字和保留字

---
+++ 第3章-语言基础/第2节-关键字与保留字/3-2-关键字与保留字.js
@@ -0,0 +1,44 @@
+
+console.log(`
+    
+    3.2 关键字与保留字
+
+        ECMA-262 描述了一组保留的关键字，这些关键字由特殊用途，比如表示控制语句
+    的开始和结束，或者执行特定的操作。按照规定，保留的关键字不能用作标识符或属性名。
+    ECMA-262 现在规定的所有关键字如下：
+        await、break、case、catch、class、const、continue、
+        debugger、default、delete、do、else、enum、export、
+        extends、false、finally、for、function、if、import、
+        in、instanceof、new、null、return、super、switch、
+        this、throw、true、try、typeof、var、void、while、
+        with、yield
+
+
+        在上下文不允许作为标识符的那些：
+        let、static、implements、interface、package、
+        private、protected、public
+
+    来自 ECMA-262 官方定义的：12.7.2 Keywords and Reserved Words 
+
+
+        模块的导入导出：import、export
+
+        函数的声明：function
+
+        逻辑控制：if、else、switch、case、default、break、return、
+    with、true、false
+
+        循环：for、do、while、in、of
+
+        变量的声明：let、const、var
+
+        类的声明：public、private、protected、interface、class、package、
+    enum、implements、extends、this、super、new
+
+        异常和捕获：try、catch、finally、throw
+
+        其他的：instanceof、typeof、void、null、yield、delete、debugger
+
+
+    
+    `);
