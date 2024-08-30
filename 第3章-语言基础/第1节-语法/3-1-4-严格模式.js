9532563d1c1fc971245d34e7ba528c86f76747b7
liuyy <liuyychn@outlook.com>
2024-08-30 11:36:26 +0800

    3.1 语法的严格模式 和 语句

---
+++ 第3章-语言基础/第1节-语法/3-1-4-严格模式.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function doSometing(){
+    "use strict";
+}
+
+console.log(`
+    
+    3.1.4 严格模式
+
+        ECMAScript 5 新增了严格模式（strict mode）的概念。
+
+        通过在 js 文件开头添加 "use strict";
+    
+    `);
