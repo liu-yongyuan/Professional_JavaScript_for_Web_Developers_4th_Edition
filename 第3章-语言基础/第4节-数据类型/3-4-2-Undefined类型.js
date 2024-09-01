6671adda903e94ee2f1d666d1add94e5a256f550
liuyy <liuyychn@outlook.com>
2024-09-01 11:11:37 +0800

    undefined 类型

---
+++ 第3章-语言基础/第4节-数据类型/3-4-2-Undefined类型.js
@@ -0,0 +1,19 @@
+console.log(`
+    
+    3.4.2 Undefined 类型
+
+    1、声明变量时默认值为 undefined
+    2、声明变量是可以赋值为 undefined
+    3、if 逻辑判断 undefined 为 false
+
+    `);
+
+
+
+
+let message;
+
+const age = undefined;
+
+console.log(typeof age);
+console.log(typeof message);
