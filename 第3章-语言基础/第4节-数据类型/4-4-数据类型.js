3c9eb887caa31dd4ccd26d0a25d289cceee08ffe
liuyy <liuyychn@outlook.com>
2024-09-01 10:53:28 +0800

    4-4-数据类型声明

---
+++ 第3章-语言基础/第4节-数据类型/4-4-数据类型.js
@@ -0,0 +1,9 @@
+console.log(`
+    
+    3.4 数据类型
+
+        ECMAScript 有 7 种简单数据类型（也称为原始类型）：Undefined、Null、
+    Boolean、Number、String、Symbol 和 BigInt。Symbol（符号）是ECMAScript6 新增的。
+    还有一种复杂数据类型叫 Object。
+
+    `);
