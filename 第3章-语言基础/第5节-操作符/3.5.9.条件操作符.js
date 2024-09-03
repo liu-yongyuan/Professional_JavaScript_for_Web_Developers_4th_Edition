42891af275338d27c28392b6e51e14d9ff3cb7e5
liuyy <liuyychn@outlook.com>
2024-09-03 16:52:21 +0800

    条件操作符，赋值操作符，逗号操作符

---
+++ 第3章-语言基础/第5节-操作符/3.5.9.条件操作符.js
@@ -0,0 +1,9 @@
+console.log(`
+    3.5.9 条件操作符
+    
+        条件操作符是 ECMAScript 中用途最为广泛的操作符之一。
+        variable = boolean_expression ? true_value : false_value
+    `);
+let max = 5 > 2 ? 1 : 2;
+// 1
+console.log(max);
