b4f91f34db567151eb2f40cde871068e7cf2cf59
liuyy <liuyychn@outlook.com>
2024-09-03 21:08:21 +0800

    3.8 小结

---
+++ 第3章-语言基础/第8节-小结/3.8.小结.js
@@ -0,0 +1,22 @@
+console.log(`
+
+    3.8 小结
+        JavaScript 的核心语言特性在 ECMA-262 中以伪语言 ECMAScript 的形式
+    来定义。 ECMAScript 包含所有基本语法、操作符、数据类型和对象，能完成基本的计算任务，
+    但没有提供获得输入和产生输出的机制。理解 ECMASCript 及其复杂的细节是完全理解
+    浏览器中 JavaScript 的关键。下面总结一下 ECMAScript 中的基本元素。
+        · ECMAScript 中的基本数据类型包括：Undefined、Null、Boolean、Number、String、
+    Symbol 和 BigInt
+        · 与其他语言不同，ECMAScript 不区分整数和浮点值，Number 可以为整数和浮点数，
+    BigInt 为正整数。
+        · Object 是一种发u咋数据类型，它是这门语言中的所有对象的基类。
+        · 严格模式为这门语言中某些容易出错的部分施加了限制。
+        · ECMAScript 提供了常见的基本操作符，包括数学操作符、布尔操作符、关系操作符、
+    相等操作符、赋值操作符和控制操作符等。
+        · 这门语言流控制语句大多是从其他语言借鉴而来的，比如 if 语句、for 语句和
+    switch 语句等。
+        · ECMAScript 中的函数与其他语言的函数不一样。
+        · 不需要指定函数的返回值，因为任何函数可以在任何时候返回值。
+        · 不指定返回值的函数实际上会返回特殊值 undefined
+
+    `);
