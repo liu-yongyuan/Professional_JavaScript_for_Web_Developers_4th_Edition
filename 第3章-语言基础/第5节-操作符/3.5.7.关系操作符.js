9bc95957da3aee2a4e043ff6e103a586ad25dd70
liuyy <liuyychn@outlook.com>
2024-09-03 16:15:55 +0800

    关系操作符

---
+++ 第3章-语言基础/第5节-操作符/3.5.7.关系操作符.js
@@ -0,0 +1,16 @@
+console.log(`
+    
+    3.5.7 关系操作符
+
+        关系操作符执行比较两个值的操作，包括小于（<）、大于（>）、
+    小于等于（<=）和大于等于（>=）
+
+    `);
+console.log(5 > 3);
+console.log(5 < 3);
+
+console.log("Brick" < "alphabet");
+
+console.log("Brick".toLowerCase() < "alphabet".toLowerCase());
+
+console.log("23" < 3);
