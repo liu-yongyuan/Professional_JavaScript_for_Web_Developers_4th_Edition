ff3c284f8a9b0714c50c3f74b12fa491c631f630
liuyy <liuyychn@outlook.com>
2024-09-06 19:52:41 +0800

    5.1.2 格式化日期

---
+++ 第5章-基本引用类型/第1节-Date/5.1.2.日期格式化.js
@@ -0,0 +1,12 @@
+console.log(`
+
+    5.1.2 日期格式化方法
+
+        Date 类型有几个专门用于格式化日期的方法，它们都会返回字符串：
+        · toDateString() 显示日期中的周几、月、日、年
+        · toTimeString() 显示日期中的时、分、秒和时区
+        · toLocaleDateString() 显示日期中的周几、月、日、年
+        · toLocaleTimeString() 显示日期中的时、分、秒
+        · toUTCString() 显示完整的 UTC 日期
+
+    `)
