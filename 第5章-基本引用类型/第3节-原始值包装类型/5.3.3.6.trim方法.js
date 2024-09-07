a4482d5e35b61f49dd564fe04c2b0bfc5cd1f3b7
liuyy <liuyychn@outlook.com>
2024-09-08 00:42:36 +0800

    trim方法

---
+++ 第5章-基本引用类型/第3节-原始值包装类型/5.3.3.6.trim方法.js
@@ -0,0 +1,15 @@
+console.log(`
+
+    6. trim() 方法
+
+        ECMAScript 在所有字符串上都提供了 trim（）方法。
+    这个方法会创建字符串的要给副本，删除前后所有空格，在返回结果。
+
+    `);
+
+let stringVale = "  hello world  ";
+let trimmedStringValue = stringVale.trim();
+//   hello world  
+console.log(stringVale);
+// hello world
+console.log(trimmedStringValue);
