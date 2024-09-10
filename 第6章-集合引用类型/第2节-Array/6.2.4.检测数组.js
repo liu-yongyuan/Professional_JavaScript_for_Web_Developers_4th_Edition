7ca87597e7c65cbe501b1b34057cc8bf2d521055
liuyy <liuyychn@outlook.com>
2024-09-10 18:04:36 +0800

    检测数组

---
+++ 第6章-集合引用类型/第2节-Array/6.2.4.检测数组.js
@@ -0,0 +1,14 @@
+console.log(`
+
+    6.2.4 检测数组
+
+        · instanceOf Array
+        · Array.isArray
+
+`);
+
+let value = [];
+// true
+console.log(value instanceof Array);
+// true
+console.log(Array.isArray(value));
