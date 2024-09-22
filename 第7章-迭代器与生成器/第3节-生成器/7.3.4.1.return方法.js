d64845921201591dc2021560faaacd177abf6c67
liuyy <liuyychn@outlook.com>
2024-09-22 22:27:11 +0800

    7.3.4.1 return 方法

---
+++ 第7章-迭代器与生成器/第3节-生成器/7.3.4.1.return方法.js
@@ -0,0 +1,63 @@
+import { logSpace } from "../../lib/utils.js";
+
+console.log(`
+
+    7.3.4 
+        1.return()
+        return() 方法会强制生成器进入关闭状态。提供给 return() 方法的值，就是终止迭代器对象的值
+`);
+function* generatorFn() {
+    for (const x of [1, 2, 3]) {
+        yield x;
+    }
+}
+
+const g = generatorFn();
+// generatorFn {<suspended>}
+console.log(g);
+// { value: 4, done: true }
+console.log(g.return(4));
+// { value: undefined, done: true }
+console.log(g.next());
+// generatorFn {<closed>}
+console.log(g);
+
+logSpace();
+
+console.log(`
+    
+        与迭代器不同，所有生成器对象都有 return() 方法，只要通过它进入关闭状态，就无法恢复了。
+    后续调用 next() 会显示 done: true 状态，而提供的任何返回值都不会被存储或传播。
+
+`);
+
+const g1 = generatorFn();
+// { value: 1, done: false }
+console.log(g1.next());
+// { value: 4, done: true }
+console.log(g1.return(4));
+// { value: undefined, done: true }
+console.log(g1.next());
+// { value: undefined, done: true }
+console.log(g1.next());
+
+logSpace();
+
+console.log(`
+
+        for-of 循环等内置语言结构会忽略状态为 done:true 的 IteratorObject 内部返回的值。
+`);
+const g2 = generatorFn();
+/* 
+1
+2
+3
+ */
+for (const x of g2) {
+    if (x > 1) {
+        g.return(4);
+    }
+    console.log(x);
+}
+
+logSpace();
